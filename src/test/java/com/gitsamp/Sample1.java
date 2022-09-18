70e7f71110299d1724980945967573ba757054d0
Louis2203 <nevinsoncruz709@gmail.com>
2022-09-18 14:10:15 +0530

    louis

---
+++ src/test/java/com/gitsamp/Sample1.java
@@ -0,0 +1,15 @@
+package com.gitsamp;
+
+public class Sample1 {
+	private void m1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m2() {
+		// TODO Auto-generated method stub
+
+	}private void m3() {
+		// TODO Auto-generated method stub
+
+	}
+}
